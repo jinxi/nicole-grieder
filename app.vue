06868e059e702d08c8cd5ca3ef20a7dd317e7cb2
jinxi <jinxi.wow@gmail.com>
2024-02-14 20:54:47 +0100

    [LOGO] ratio for small screens

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts">
-const version = 2 + 1
-</script>
-
-<template>
+<!-- <head>
+  <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js" defer></script>
+  <script src="assets/js/app.js" defer></script>
+</head> -->
+<template id="app">
   <section class="section_container gap">
     <div class="default section_layout">
       <div class="rowLayout">
@@ -11,8 +13,9 @@ const version = 2 + 1
         <div class="content gap">
           <div>
             <h1>Hallo lieber Mensch!</h1>
+            <!-- {{data.email}} -->
             <p>Willkommen auf meiner noch sehr spaerlichen Website, schoen hast du mich gefunden. Wie du moeglicherweise schon erkennst, ist die Seite noch im Aufbau.</p>
-            <p>Bitte nimm aber gerne mit mir kontakt auf: <a href="mailto: coaching@nicole-grieder.ch" target="_self"> coaching@nicole-grieder.ch</a></p>
+            <p>Bitte nimm aber gerne mit mir kontakt auf: <a href="mailto:coaching@nicole-grieder.ch" target="_self"> coaching@nicole-grieder.ch</a></p>
           </div>
         </div>
       </div>
@@ -26,11 +29,11 @@ const version = 2 + 1
 <style scoped>
 .header{
   width: 100%;
-  height: 200px;
+  height: 160px;
+  background-size: contain ;    
   background-image: url("assets/img/LogoTransparent.png");
   background-repeat: no-repeat;
   background-position: center;
-  /* background-color: var(--color-primary); */
 }
 .content{
   width: 100%;
@@ -49,4 +52,13 @@ div{
   /* border: 1px solid black; */
 }
 
+/* @media (max-width: 1000px) {
+  .header{
+  width: 212px;
+  height: 53px;
+  background-image: url("assets/img/LogoTransparent (media).png");
+  background-repeat: no-repeat;
+  background-position: center;
+} 
+}*/
 </style>
